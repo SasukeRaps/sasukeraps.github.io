01738fcd7ffb10e429de38578c52e0b99a233118
SasukeRaps <sasukerapz@gmail.com>
2023-02-21 12:23:43 -0600

    Web

--- script.js
+++ script.js
@@ -21,16 +21,18 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
+        habilidades[0].classList.add("java");
         habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("photoshop");
         habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[4].classList.add("sql");
+        habilidades[5].classList.add("flstudio");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("trabajo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("proyects");
+       
     }
 }
 
